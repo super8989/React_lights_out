298eb0184ed2bb3806cc369192637b47b92e3b92
Sam <super8989@gmail.com>
2019-10-17 21:57:13 -0400

    added in the starter codes

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,59 @@
 import React, { Component } from "react";
 import Cell from './Cell';
 
+
+/** Game board of Lights out.
+ *
+ * Properties:
+ *
+ * - nrows: number of rows of board
+ * - ncols: number of cols of board
+ * - chanceLightStartsOn: float, chance any cell is lit at start of game
+ *
+ * State:
+ *
+ * - hasWon: boolean, true when board is all off
+ * - board: array-of-arrays of true/false
+ *
+ *    For this board:
+ *       .  .  .
+ *       O  O  .     (where . is off, and O is on)
+ *       .  .  .
+ *
+ *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
+ *
+ *  This should render an HTML table of individual <Cell /> components.
+ *
+ *  This doesn't handle any clicks --- clicks are on individual cells
+ *
+ **/
+
+
 class Board extends Component {
+  
+  constructor(props) {
+    super(props);
+  }
+
+  createBoard() {
+    let board = [];
+    return board;
+  }
+
+  flipCellsAround(coord) {
+    let { ncols, nrows } = this.props;
+    let board = this.state.board;
+    let [y, x] = coord.split("-").map(Number);
+
+    function flipCell(y, x) {
+
+      if ( x >= 0 && x < ncols && y >=0 && y < nrows ) {
+        board[y][x] = !board[y][x];
+      }
+    }
+  }
+
+
 	render() {
 		return (
       <table>
