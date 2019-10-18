485b2f8372fc5371f460fd54294138c2abd9832f
Sam <super8989@gmail.com>
2019-10-17 21:06:13 -0400

    created cell component

--- src/Board.js
+++ src/Board.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Cell from './Cell';
 
 class Board extends Component {
 	render() {
 		return (
 			<div>
-				<h1>Board</h1>
+				<Cell />
 			</div>
 		);
 	}
---
+++ src/Cell.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Cell extends Component {
+	render() {
+		return (
+      <td />
+    )
+	}
+}
+
+export default Cell;
